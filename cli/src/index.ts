d5d49431b361b664362f5a14c0c16208ebbbfe26
Fernando Salazar <fslzrr@gmail.com>
2020-10-16 15:59:47 -0600

    :tada: created cli initial structure

---
+++ cli/src/index.ts
@@ -0,0 +1,32 @@
+import ora from "ora";
+import { resolve } from "path";
+import yargs from "yargs";
+
+async function init() {
+  // TODO: define several arguments to add more optional configurations
+  const args = yargs.options({
+    _: { type: "string", demandOption: true, alias: "f" },
+  }).argv;
+
+  // TODO: valite file extension - support for modules?
+  const [file] = args._;
+  if (!file) {
+    console.error("You must provide a file.");
+    process.exit(1);
+  }
+
+  const spinner = ora(`Compiling file: ${file}`).start();
+  // TODO: use compiler
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+  await promise;
+  spinner.stop();
+
+  console.log(`Successfully compiled!`);
+  process.exit(0);
+}
+
+init();
